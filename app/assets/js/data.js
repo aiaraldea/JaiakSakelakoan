f607d3191be75c75e7f438dba86b3183af51c318
Iñaki Ibarrola Atxa <inakiibarrola@yahoo.es>
2012-07-20 04:17:29 -0400

    Ezabatu Urduñako jaien deialdiak

--- app/assets/js/data.js
+++ app/assets/js/data.js
@@ -1,356 +1,109 @@
 var data = {
-    "izena": "Amurrioko jaiak 2012",
-    "kartelarenEgilea": "",
-    "egunak": [
+  "izena": "Amurrioko jaiak 2012",
+  "kartelarenEgilea": "",
+  "egunak": [
     {
-        "id": 1,
-        "data": "2012-08-04",
-        "izena": "",
-        "deialdiak": [
+      "id": 1,
+      "data": "2012-08-04",
+      "izena": "",
+      "deialdiak": [
         {
-            "id": 0,
-            "ordua": "18:00",
-            "izenburua": "Andra Mariaren Elizara SANTUAREN JAITSIERA",
-            "xehetasunak": "Done Errokeren baselizatik Andra Mariaren Elizara SANTUAREN JAITSIERA. Antolatzailea: Vera Cruz Kofradia. Autobusak:  17:30 eta 17:45etan: Elizako errotondako bus geltokian, 17:35 eta  17:50etan Aretxondo Tabernan."
+          "id": 0,
+          "ordua": "18:00",
+          "izenburua": "Andra Mariaren Elizara SANTUAREN JAITSIERA",
+          "xehetasunak": "Done Errokeren baselizatik Andra Mariaren Elizara SANTUAREN JAITSIERA. Antolatzailea: Vera Cruz Kofradia. Autobusak:  17:30 eta 17:45etan: Elizako errotondako bus geltokian, 17:35 eta  17:50etan Aretxondo Tabernan."
         }
-        ]
+      ]
     },
     {
-        "id": 2,
-        "data": "2012-08-10",
-        "izena": "Txupinazoa",
-        "deialdiak": [
+      "id": 2,
+      "data": "2012-08-10",
+      "izena": "Txupinazoa",
+      "deialdiak": [
         {
-            "id": 1,
-            "ordua": "09:30-13:30",
-            "izenburua": "Odol ematea",
-            "xehetasunak": "Jaiei elkartasunez ekingo diegu odola emateko autobusean, Larrinagako oinezkoen aldean."
+          "id": 1,
+          "ordua": "09:30-13:30",
+          "izenburua": "Odol ematea",
+          "xehetasunak": "Jaiei elkartasunez ekingo diegu odola emateko autobusean, Larrinagako oinezkoen aldean."
         },
         {
-            "id": 2,
-            "ordua": "11:00",
-            "izenburua": "'AMURRIOKO HIRIBILDUA' PILOTA TXAPELKETAko Finalerdiak, Udal Pilotalekuan.",
-            "xehetasunak": ""
+          "id": 2,
+          "ordua": "11:00",
+          "izenburua": "'Amurrioko Hiribildua' <b>pilota txapelketa</b>ko finalerdiak, Udal Pilotalekuan.",
+          "xehetasunak": ""
         },
         {
-            "id": 3,
-            "ordua": "13:00",
-            "izenburua": "KOADRILEN AURKEZPENA Juan Urrutia plazan.",
-            "xehetasunak": ""
+          "id": 3,
+          "ordua": "13:00",
+          "izenburua": "<b>Koadrilen aurkezpena</b> Juan Urrutia plazan.",
+          "xehetasunak": ""
         },
         {
-            "id": 92,
-            "ordua": "18:00",
-            "izenburua": "Udaletxeko Aretoan  JAIETAKO PREGOIAREN IRAKURKETA, MENDIKO LAGUNAK ELKARTEAREN eskutik."
+          "id": 92,
+          "ordua": "18:00",
+          "izenburua": "Udaletxeko aretoan  jaietako&nbsp;<b>pregoaiaren irakurketa</b>, <i>Mendiko Lagunak</i> elkartearen eskutik."
         },
         {
-            "id": 93,
-            "ordua": "19:00",
-            "izenburua": "Txupinazoa",
-            "xehetasunak": "Herriko Jaietako TXUPINAZOA airera bota eta IGUARRAKOAren jaitsiera, 'EUSKOTARRAK', 'DANTZA LAGUNAK', 'TROT-ART', 'EL BOLI' eta 'HERRIARENAK' koadrilen kalejira, BUILAKA perkusio taldeak eta erraldoi bikoteak lagundurik."
+          "id": 93,
+          "ordua": "19:00",
+          "izenburua": "Txupinazoa",
+          "xehetasunak": "Herriko Jaietako <b>txupinazoa</b> airera bota eta <b>Iguarrako</b>ren jaitsiera, '<i>Euskotarrak</i>', '<i>Dantza Lagunak</i>', '<i>Trot-art</i>', '<i>El Boli</i>' eta '<i>Herriarenak</i>' koadrilen kalejira, BUILAKA perkusio taldeak eta erraldoi bikoteak lagundurik."
         },
         {
-            "id": 94,
-            "ordua": "22:30",
-            "izenburua": "PROFESIONALEN PILOTA PARTIDUA",
-            "xehetasunak": "Udal Pilotalekuan"
+          "id": 94,
+          "ordua": "22:30",
+          "izenburua": "Profesionalen <b>pilota partidua</b>",
+          "xehetasunak": "Udal Pilotalekuan"
         },
         {
-            "id": 95,
-            "ordua": "22:30",
-            "izenburua": "Berbena LISKER taldearekin",
-            "xehetasunak": "Juan Urrutia plazan"
+          "id": 95,
+          "ordua": "22:30",
+          "izenburua": "<b>Berbena</b> <i>Lisker</i> taldearekin",
+          "xehetasunak": "Juan Urrutia plazan"
         }
-        ]
+      ]
     },
     {
-        "id": 3,
-        "data": "2012-08-11",
-        "izena": "Haur eta atzoko gazteen eguna",
-        "deialdiak": [
-        {
-            "id": 4,
-            "ordua": "09:00",
-            "izenburua": "Zezen-plazaren inguruan, <strong>Euskadiko Txarolen IX. Txapelketa</strong>. (aparteko egitaraua ikusi).",
-            "xehetasunak": "Goizean, erakustazokan:     <ul>       <li>nekazaritzako makinen erakusketa eta salmenta.</li>       <li>bertoko produktuen erakusketa eta salmenta.</li>       <li>bertoko musika.</li>      </ul>                              "
-        },
-        {
-            "id": 5,
-            "ordua": "13:00",
-            "izenburua": "Urduñako Santa Zezilia musika-bandaren <strong>kontzertua</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 6,
-            "ordua": "18:00",
-            "izenburua": "Udal-bolatokian, Otxomaioetako XVII. Lurralde Lehiaketako <strong>bolo jaurtiketa</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 7,
-            "ordua": "18:30",
-            "izenburua": "Pilotalekuan,  <strong>gimnasia erritmikoko erakustaldia</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 8,
-            "ordua": "20:00",
-            "izenburua": "Dantzaldia <strong>Joselu Anaiak</strong> taldearekin.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 9,
-            "ordua": "20:30",
-            "izenburua": "Andra Maria parrokian, <strong>abesbatzen kontzertuak</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 10,
-            "ordua": "21:30",
-            "izenburua": "Foru plazan <strong>III. play back lehiaketa</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 11,
-            "ordua": "22:00",
-            "izenburua": "Herri-eskolako jolastokian, Gazte Asanbladak gazteentzat antolatutako <strong>kontzertu autokudeatuak</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 12,
-            "ordua": "01:00",
-            "izenburua": "Foru plazan, animazioa <strong>Juanma DJrekin</strong>",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 3,
+      "data": "2012-08-11",
+      "izena": "Haur eta atzoko gazteen eguna",
+      "deialdiak": []
     },
     {
-        "id": 4,
-        "data": "2012-08-12",
-        "izena": "Kultura eta kirolaren eguna",
-        "deialdiak": [
-        {
-            "id": 13,
-            "ordua": "10:00",
-            "izenburua": "Urduñako Hiriko <strong>Ibilgailu Klasiko eta Historikoen I. Kontzentrazioa</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 14,
-            "ordua": "11:00",
-            "izenburua": "Udal-bolatokian, Otxomaioetako XVII. Lurralde Lehiaketako <strong>bolo jaurtiketa</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 15,
-            "ordua": "11:30",
-            "izenburua": "Foru plazan, Urduñako usainen, testuren eta zaporeen bost zentzumeneko <strong>tailerrak</strong> txikinenentzat.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 16,
-            "ordua": "13:00",
-            "izenburua": "Foru plazan, <strong>txistuen alardea</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 17,
-            "ordua": "18:00",
-            "izenburua": "<strong>Zezenketa mistoa</strong> Cadizko Gavira ganadutegiko 4 zezen eta 2 zekor, tokian tokiko zezenketari hauentzat:",
-            "xehetasunak": "<ul>       <li>Ivan Fandiño</li>       <li>Finito De Cordoba</li>       <li>Ivan Abasolo</li>     </ul>                              "
-        },
-        {
-            "id": 18,
-            "ordua": "19:00",
-            "izenburua": "Foru plazan, Umore-ikuskizuna <strong>Gorka Aginagalde</strong>rekin.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 19,
-            "ordua": "21:00",
-            "izenburua": "Foru plazan, dantzaldia <strong>Arizgoiti</strong> taldearekin.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 4,
+      "data": "2012-08-12",
+      "izena": "Kultura eta kirolaren eguna",
+      "deialdiak": []
     },
     {
-        "id": 5,
-        "data": "2012-08-13",
-        "izena": [],
-        "deialdiak": [
-        {
-            "id": 20,
-            "ordua": "22:00",
-            "izenburua": "Foru plazan, dantzaldia <strong>Hotza</strong> taldearekin.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 5,
+      "data": "2012-08-13",
+      "izena": [],
+      "deialdiak": []
     },
     {
-        "id": 6,
-        "data": "2012-08-14",
-        "izena": "",
-        "deialdiak": [
-        {
-            "id": 21,
-            "ordua": "10:30",
-            "izenburua": "<strong>Urduñako 'Antiguako' Andra Mariari omenaldia</strong> Urduñako hiriko udal-korporazioa eta Ruzabaleko Batzarreko auzo-alkateak mazolariekin eta Klarinen Bandarekin joango dira Antiguako santutegira, bertako zaindaria eta titularra den Urduñako 'Antiguako' Andra Maria omentzeko.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 22,
-            "ordua": "11:00",
-            "izenburua": "Antiguako santutegian, <strong>meza nagusia</strong>. Mezatik irtendakoan, <strong>ohorezko aurreskua</strong> dantzatuko da.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 23,
-            "ordua": "12:30",
-            "izenburua": "<strong>Erraldoi eta buruhandien kalejira</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 24,
-            "ordua": "13:00",
-            "izenburua": "Antiguako santutegian, <strong>meza</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 25,
-            "ordua": "13:30",
-            "izenburua": "Foru plazan, <strong>Santa Zezilia bandaren kontzertua</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 26,
-            "ordua": "18:30",
-            "izenburua": "Foru plazan, umeentzako <strong>zirko-tailerra Zurrumurru</strong> taldearekin.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 27,
-            "ordua": "20:30",
-            "izenburua": "Dantzaldia <strong>Kosmos</strong> taldearekin.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 28,
-            "ordua": "22:30",
-            "izenburua": "Foru plazan, <strong>Deabru Beltzak</strong> taldearen ikuskizuna.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 6,
+      "data": "2012-08-14",
+      "izena": "",
+      "deialdiak": []
     },
     {
-        "id": 7,
-        "data": "2012-08-15",
-        "izena": "Ama Birjinaren eguna",
-        "deialdiak": [
-        {
-            "id": 29,
-            "ordua": "11:00",
-            "izenburua": "Udaletxeko batzar-aretoan, Otxomaioetako Kartelen Lehiaketako <strong>sarien banaketa</strong> , umeen zein kimuen kategorietan.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 30,
-            "ordua": "11:15",
-            "izenburua": "<strong>TXIKI TXUPIA</strong>, 2012ko Otxomaioetako txiki txupineroaren eskutik.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 31,
-            "ordua": "11:30",
-            "izenburua": "Arrastarriako Batzarretako agintariek hiria zeharkatuko dute Antiguako santutegirako <strong>erromesaldia</strong>n, bere garaian antzina-antzinatik euren zaindaria denari egindako botoa betetzeko.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 32,
-            "ordua": "12:00",
-            "izenburua": "Antiguako santutegian, <strong>meza</strong>. Amaitutakoan, entradillen dantza tradizionala.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 33,
-            "ordua": "11:30-14:30",
-            "izenburua": "Foru plazan, <strong>puzgarrien parkea</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 34,
-            "ordua": "14:30",
-            "izenburua": "<strong>Umeentzako entzierroa</strong> udaletxearen beheko aldetik Gernika plazaraino.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 35,
-            "ordua": "15:00",
-            "izenburua": "Gernika plazan, <strong>umeentzako herri-bazkaria</strong> (prezioa: 3,50 e).",
-            "xehetasunak": "Ticketak apirilaren 25ean, 26an eta 27an erosi ahal izango dira Kultur Etxean, 17:00etatik 19:00etara.              "
-        },
-        {
-            "id": 36,
-            "ordua": "17:30",
-            "izenburua": "Gernika plazan, <strong>umeentzako dantzaldia Gramola</strong> taldearekin.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 37,
-            "ordua": "19:30",
-            "izenburua": "Gernika plazan, <strong>txapel jaurtiketaren V. txapelketa</strong>.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 7,
+      "data": "2012-08-15",
+      "izena": "Ama Birjinaren eguna",
+      "deialdiak": []
     },
     {
-        "id": 8,
-        "data": "2012-08-16",
-        "izena": "Done Erroke",
-        "deialdiak": [
-        {
-            "id": 38,
-            "ordua": "12:30",
-            "izenburua": "Andra Maria parrokian, <strong>meza</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 39,
-            "ordua": "14:30",
-            "izenburua": "Udal-pilotalekuan, <strong>bazkaria</strong> Prezioa: 6 e. Ticketak maiatzaren 2tik aurrera erosi ahal izango dira Kultur Etxean, astelehenetik ostiralera, 9:00etatik 13:00era. Inskripzio mugatuak.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 40,
-            "ordua": "17:00",
-            "izenburua": "Bazkalosteko alaia <strong>Campanera</strong> taldearekin.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 8,
+      "data": "2012-08-16",
+      "izena": "Done Erroke",
+      "deialdiak": []
     },
     {
-        "id": 9,
-        "data": "2012-08-17",
-        "izena": [],
-        "deialdiak": [
-        {
-            "id": 41,
-            "ordua": "17:00",
-            "izenburua": "Udal-pilotalekuan, <strong>saskibaloiko partidak</strong> (aparteko egitaraua ikusi).",
-            "xehetasunak": ""
-        },
-        {
-            "id": 42,
-            "ordua": "22:00",
-            "izenburua": "Foru plazan, <strong>lagun-taldeen afaria</strong>.",
-            "xehetasunak": ""
-        },
-        {
-            "id": 43,
-            "ordua": "23:30",
-            "izenburua": "Dantzaldia <strong>Izargi</strong> taldearekin.",
-            "xehetasunak": ""
-        }
-        ]
+      "id": 9,
+      "data": "2012-08-17",
+      "izena": [],
+      "deialdiak": []
     }
-    ]
+  ]
 }
